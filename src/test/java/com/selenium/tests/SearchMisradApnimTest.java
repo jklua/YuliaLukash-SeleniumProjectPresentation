da4dab30c80b08ef5cc2bc9786e1dfd989761ee4
jklua <yulia.lukashevich@gmail.com>
2020-09-16 21:05:50 +0300

    Created test

---
+++ src/test/java/com/selenium/tests/SearchMisradApnimTest.java
@@ -0,0 +1,40 @@
+package com.selenium.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SearchMisradApnimTest {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void setUp(){
+       wd = new FirefoxDriver();
+       // wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        wd.navigate().to("https://www.gov.il/");
+
+    }
+    @Test
+    public void searchFromNavMenuTest(){
+        //click on Kabalat Kahal קבלת קהל
+        wd.findElement(By.cssSelector("#tm-4")).click();
+
+
+        String pageTitle = wd.findElement(By.cssSelector("h1 span")).getText();
+        System.out.println(pageTitle);
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        wd.quit();
+
+    }
+}
